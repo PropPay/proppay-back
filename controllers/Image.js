8bd10dd43420e3978636ca994fe1090a15a2d93b
Coulibaly Nahoua Aruna <nahoua.c@geodaftar.com>
2024-01-26 16:56:57 +0000

    image storage ok

--- controllers/Image.js
+++ controllers/Image.js
@@ -1,29 +1,7 @@
 import fs from "fs";
 import multer from "multer";
-import path, { dirname } from 'path';
 import tmp from "tmp";
-import { fileURLToPath } from 'url';
 import Image from '../models/Image.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-//const uploadDir = path.join(os.tmpdir(), 'uploads');
-
-/* if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-} */
-
-/* const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    }
-});
-
-export const upload = multer({ storage: storage }); */
 
 // Configurations Multer pour le stockage des fichiers temporaires
 const storage = multer.memoryStorage(
@@ -35,35 +13,15 @@ const storage = multer.memoryStorage(
             cb(null, file.originalname);
         }
     });
+
 export const upload = multer({ storage: storage });
 
 const postImage = (async (req, res) => {
     try {
-        /* const imagePath = req.file ? req.file.path : null;
-        console.log(imagePath);
-        if (!imagePath) {
-            return res.status(400).send('Aucun fichier image téléchargé.');
-        }
-
-        const { title, description } = req.body; // Ajoutez les autres données ici
-
-        // Convertissez le chemin d'accès en base 64
-        const imagePathBase64 = Buffer.from(imagePath).toString('base64');
-        console.log(imagePathBase64);
-        // Enregistrez le chemin d'accès imagePathBase64 et les autres données dans MongoDB */
+        
         const { title, description } = req.body;
         const tmpFile = tmp.fileSync();
         fs.writeFileSync(tmpFile.name, req.file.buffer);
-
-        /* const tempDir = await fs.mkdtemp(path.join('/tmp', 'uploads-'));
-        const tempFilePath = path.join(tempDir, req.file.originalname);
-
-        await fs.writeFile(tempFilePath, req.file.buffer); */
-
-        // Faites quelque chose avec le fichier temporaire, par exemple, le stocker en base de données
-
-        // Supprimez le répertoire temporaire après utilisation  await fs.rmdir(tempDir, { recursive: true });
-       
 
         const image = new Image({
             imagePath: req.file.originalname,
@@ -75,6 +33,7 @@ const postImage = (async (req, res) => {
         await image.save();
 
         res.send('Image et données téléchargées avec succès.');
+        res.status(201).json({ filename: req.file.originalname })
     } catch (error) {
         console.error(error);
         res.status(500).send('Erreur lors du téléchargement de l\'image et des données.');
@@ -84,17 +43,12 @@ const postImage = (async (req, res) => {
 const getImage = (async (req, res) => {
     try {
         const image = await Image.findById(req.params.id);
-
         if (!image || !image.imagePath) {
             return res.status(404).send('Image non trouvée.');
         }
 
-        // Construisez le chemin absolu en utilisant path.join
-        const absoluteImagePath = path.join(__dirname.substring(0, 36), image.imagePath);
-
-        // Envoyez l'image en tant que réponse
-        console.log(absoluteImagePath);
-        res.sendFile(absoluteImagePath);
+        res.set('Content-Type', 'image/jpeg'); // Assurez-vous de définir le type MIME approprié
+        res.send(image.data);
     } catch (error) {
         console.error(error);
         res.status(500).send('Erreur lors de la récupération de l\'image.');
